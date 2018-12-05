2c77b3f1d3f3abaaf7d501c584f52fa776d63c08
mkobierecki <mkobierecki@pretius.pl>
2018-12-05 06:03:49 +0100

    security config refactor

--- src/main/java/com/student/integration/config/SecurityConfig.java
+++ src/main/java/com/student/integration/config/SecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.BeanIds;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -52,6 +53,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .authenticationProvider(facebookAuthenticationProvider)
                 .userDetailsService(siUserDetailsService);
     }
+
+    @Bean(BeanIds.AUTHENTICATION_MANAGER)
+    @Override
     public AuthenticationManager authenticationManagerBean() throws Exception{
         return super.authenticationManagerBean();
     }
